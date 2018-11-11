912d9d778f62b5a82c313e2dadb848eb076c0c0a
Tutuke <bodistundeanna@gmail.com>
2018-11-11 13:02:38 +0100

    first

---
+++ src/mainpage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-type" charset=UTF-8 " />
+<title>Football Website</title>
+<link rel="icon" href="" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="../assets/style.css">
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"
+	type="text/javascript"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+	<div id="header_img">
+			<img src="../header_ball.jpg" alt="CS Viitorul header" height="300"
+				width="1500">
+	</div>
+	<div id="header_div">
+		<div id="nav_bar">
+			<ul>
+				<li><a href="mainpage.php">Főoldal</a></li>
+				<li><a href="news.php">Hírek</a></li>
+				<li><a href="footballplay.php">Labdarúgás</a></li>
+				<li><a href="aboutus.php">Rólunk</a></li>
+			</ul>
+		</div>
+		<div id="logo_div">
+			<img src="../logo_img.png" alt="CS Viitorul Logo" height="70"
+				width="70">
+		</div>
+	</div>
+	<div id="content_div">
+	
+	
+	</div>
+	<div id="footer_div">
+		<div id="footer_date"><?php echo date("Y")?></div>
+	</div>
+</body>
+</html>
